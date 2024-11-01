9cfe41b5ed1d4fe84425d5b2934b271cf4d38852
versiw <stao.ding@qq.com>
2024-11-01 16:14:40 +0800

    style: 测试 tailwindcss 的使用

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 import Layout from '@renderer/layouts/Layout.vue'
+import { darkTheme } from 'naive-ui'
 </script>
 
 <template>
-  <Layout></Layout>
+  <n-config-provider :theme="darkTheme" class="h-full">
+    <Layout></Layout>
+  </n-config-provider>
 </template>
--- src/renderer/src/layouts/Layout.vue
+++ src/renderer/src/layouts/Layout.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { darkTheme } from 'naive-ui'
 import { RouterView } from 'vue-router'
 import { useMenu } from '@renderer/hooks/layout/menu'
 
@@ -9,46 +8,48 @@ const isCollapsed = ref(false)
 const { activeKey, menuOptions, handleMenuClick } = useMenu()
 </script>
 <template>
-  <n-config-provider :theme="darkTheme">
-    <n-layout style="height: 100vh">
-      <n-layout-header style="height: 64px; padding: 24px" bordered>
-        Vue Electron Starter
-      </n-layout-header>
-      <n-layout position="absolute" style="top: 64px; bottom: 0px" has-sider>
-        <n-layout-sider
-          :native-scrollbar="false"
-          bordered
-          collapse-mode="width"
-          :collapsed-width="64"
-          :width="220"
+  <n-layout class="h-full">
+    <n-layout-header class="h-16 p-2" bordered>
+      <n-flex class="flex items-center h-12" justify="space-between">
+        <div>Vue Electron Starter</div>
+        <n-button>Oops!</n-button>
+        <n-button>Oops!</n-button>
+      </n-flex>
+    </n-layout-header>
+    <n-layout position="absolute" style="top: 64px; bottom: 0px" has-sider>
+      <n-layout-sider
+        :native-scrollbar="false"
+        bordered
+        collapse-mode="width"
+        :collapsed-width="64"
+        :width="220"
+        :collapsed="isCollapsed"
+        show-trigger="arrow-circle"
+        @collapse="isCollapsed = true"
+        @expand="isCollapsed = false"
+      >
+        <n-menu
+          v-model:value="activeKey"
+          :options="menuOptions"
           :collapsed="isCollapsed"
-          show-trigger
-          @collapse="isCollapsed = true"
-          @expand="isCollapsed = false"
+          :collapsed-width="64"
+          :collapsed-icon-size="24"
+          @update:value="handleMenuClick"
+        />
+      </n-layout-sider>
+      <n-layout :native-scrollbar="false">
+        <n-layout
+          position="absolute"
+          content-style="padding: 24px;"
+          style="top: 0px; bottom: 64px"
+          :native-scrollbar="false"
         >
-          <n-menu
-            v-model:value="activeKey"
-            :options="menuOptions"
-            :collapsed="isCollapsed"
-            :collapsed-width="64"
-            :collapsed-icon-size="24"
-            @update:value="handleMenuClick"
-          />
-        </n-layout-sider>
-        <n-layout :native-scrollbar="false">
-          <n-layout
-            position="absolute"
-            content-style="padding: 24px;"
-            style="top: 0px; bottom: 64px"
-            :native-scrollbar="false"
-          >
-            <RouterView />
-          </n-layout>
-          <n-layout-footer position="absolute" style="height: 64px; padding: 24px" bordered>
-            底部布局
-          </n-layout-footer>
+          <RouterView />
         </n-layout>
+        <n-layout-footer position="absolute" style="height: 64px; padding: 24px" bordered>
+          底部布局
+        </n-layout-footer>
       </n-layout>
     </n-layout>
-  </n-config-provider>
+  </n-layout>
 </template>
